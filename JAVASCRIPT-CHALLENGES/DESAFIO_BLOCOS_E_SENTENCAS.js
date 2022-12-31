8ab7513282148a311c140104b7d0a965768d4429
lucaskazuma <lucaskazuma@hotmail.com>
2022-12-31 00:22:16 -0300

    feat: Blocos e sentenças de código

---
+++ JAVASCRIPT-CHALLENGES/DESAFIO_BLOCOS_E_SENTENCAS.js
@@ -0,0 +1,17 @@
+{
+    console.log("Número 1");
+    console.log("Número 3");
+    console.log("Número 5");
+    console.log("Número 7");
+    console.log("Número 9");
+}
+
+{
+    console.log("Número 2");
+    console.log("Número 4");
+    console.log("Número 6");
+    console.log("Número 8");
+    console.log("Número 10");
+}
+
+console.log("FIM!");
