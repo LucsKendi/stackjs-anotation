21fa0ba2bee81957dd1f4e79b86e9f1525fc2e71
lucaskazuma <lucaskazuma@hotmail.com>
2023-01-18 20:03:09 -0300

    feat: Impressão objetos para usuário

---
+++ JAVASCRIPT-CHALLENGES/DESAFIO_IMPRESSAO_OBJETOS.js
@@ -0,0 +1,12 @@
+const company = {
+    name: "Rocketseat",
+    color: "Roxo",
+    workField: "Programação",
+    adress: {
+        street: "Rua Guilherme Gembala,",
+        number: 260
+    }
+}
+
+console.log(`A empresa ${company.name} está localizada na ${company.adress.street} ${company.adress.number}`)
+
