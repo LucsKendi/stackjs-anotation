8f9c5383bb94557e30fa0cee987de5b58c169774
lucaskazuma <lucaskazuma@hotmail.com>
2023-01-18 19:03:45 -0300

    style: Indentação

--- JAVASCRIPT-CHALLENGES/DESAFIO_NUMEROS_PARES.js
+++ JAVASCRIPT-CHALLENGES/DESAFIO_NUMEROS_PARES.js
@@ -18,17 +18,18 @@ let ajuda = 10;
 let aviso = "(" + ajuda + (" ° elemento)");
 let valor = "Valor da posição: ";
 let espaco = ("  ");
+
 for (pos = 0; pos < arrayInicial.length; pos++) {
-    if(aux == 9){
-    let array1 = valor + arrayInicial[pos] + espaco + aviso;
-    arrayDefinitiva.push(array1)
-    aux = 0;
-    ajuda = ajuda + 10;
-    aviso = "(" + ajuda + (" ° elemento)");
-    
-} else{
-    arrayDefinitiva.push(arrayInicial[pos])
-    aux++;
-}
+    if (aux == 9) {
+        let array1 = valor + arrayInicial[pos] + espaco + aviso;
+        arrayDefinitiva.push(array1)
+        aux = 0;
+        ajuda = ajuda + 10;
+        aviso = "(" + ajuda + (" ° elemento)");
+
+    } else {
+        arrayDefinitiva.push(arrayInicial[pos])
+        aux++;
+    }
 }
 console.log(arrayDefinitiva);
