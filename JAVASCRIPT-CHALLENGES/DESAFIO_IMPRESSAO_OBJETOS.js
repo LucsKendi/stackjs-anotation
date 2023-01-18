b18bdb7c1db52dc63420c1de1f4074cd2fd1c238
lucaskazuma <lucaskazuma@hotmail.com>
2023-01-18 19:33:34 -0300

    feat/desafio_impressao_objetos

---
+++ JAVASCRIPT-CHALLENGES/DESAFIO_IMPRESSAO_OBJETOS.js
@@ -0,0 +1,11 @@
+const empresa = {
+    name: "Rocketseat",
+    color: "Roxo",
+    workField: "Programação",
+    adress: {
+        street: "Rua Guilherme Gembala",
+        number: 260
+    }
+}
+
+console.log(`A empresa ${empresa.name} está localizada na ${empresa.adress.street}, ${empresa.adress.number}`)
